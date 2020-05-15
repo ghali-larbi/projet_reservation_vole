65321de2b950d12d2a200db89cb3ad214f954279
ghali-larbi <larbighali89@gmail.com>
2020-05-15 13:02:06 +0000

    ok

--- confirmation.php
+++ confirmation.php
@@ -27,7 +27,7 @@
 
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="connexion.php"><img src="logo3.png" width="120" height="80" alt=""></a>
+        <a class="navbar-brand" href="index.php"><img src="logo3.png" width="120" height="80" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
